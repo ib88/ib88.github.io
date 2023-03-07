88f65af2a936b69199341c6f79295ded397ac662
ib88 <imefire@gmail.com>
2023-03-07 07:48:54 +0100

    create webhook and list them

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/test/SimplyCSV_Docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/test/SimplyCSV_Docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/test/SimplyCSV_Docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/test/SimplyCSV_Docs/src/css/custom.css'),
+];
