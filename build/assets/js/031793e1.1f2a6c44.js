afbda277b8d55597587d8742e55d807e0614cb6d
ib88 <imefire@gmail.com>
2023-03-08 17:27:38 +0100

    modified the gitignore file to exclude the build

--- build/assets/js/031793e1.1f2a6c44.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksimply_csv_docs=self.webpackChunksimply_csv_docs||[]).push([[1633],{2511:s=>{s.exports=JSON.parse('{"label":"facebook","permalink":"/blog/tags/facebook","allTagsPath":"/blog/tags","count":1}')}}]);
--- build/assets/js/30a24c52.86604fb5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksimply_csv_docs=self.webpackChunksimply_csv_docs||[]).push([[453],{8605:s=>{s.exports=JSON.parse('{"label":"hello","permalink":"/blog/tags/hello","allTagsPath":"/blog/tags","count":2}')}}]);
