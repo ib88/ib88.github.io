06ead8ff13aa5fab9535d54de7f0e56811bb407f
ib88 <imefire@gmail.com>
2023-03-08 17:29:00 +0100

    added the file sections in the developer sections

---
+++ build/assets/js/e16015ca.533453ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksimply_csv_docs=self.webpackChunksimply_csv_docs||[]).push([[9700],{5688:s=>{s.exports=JSON.parse('{"label":"hola","permalink":"/blog/tags/hola","allTagsPath":"/blog/tags","count":1}')}}]);
---
+++ build/assets/js/f7c814ca.e8900fd5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksimply_csv_docs=self.webpackChunksimply_csv_docs||[]).push([[2181],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
