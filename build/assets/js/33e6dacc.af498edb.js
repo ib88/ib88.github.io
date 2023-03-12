2b2c2317d3beed5bd459559452c390d8202dc901
ib88 <imefire@gmail.com>
2023-03-12 14:06:54 +0100

    deploying the first intro and templates corrections

---
+++ build/assets/js/33e6dacc.af498edb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksimply_csv_docs=self.webpackChunksimply_csv_docs||[]).push([[1497],{4469:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
